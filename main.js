65114d8a8c2451fce7ed4f7db045e961b1901186
jacob <2627302231@qq.com>
2025-12-25 11:34:42 +0800

    add AND condition

--- main.js
+++ main.js
@@ -1,16 +1,73 @@
 const axios = require('axios');
 
-async function getLogs() {
+async function getLogs(net, message, query_period) {
   const url = 'http://log.wanchain.org:9200/_search?pretty';
-  
-  // 构造查询 DSL (Domain Specific Language)
+  // // 构造查询 DSL (Domain Specific Language)
   const queryData = {
+    size: 2, // 返回最近的 20 条
+    sort: [
+      { "@timestamp": { "order": "desc" } } // 按时间倒序
+    ],
     query: {
-      match_all: {}
-    },
-    size: 5,
-    sort: [{ "@timestamp": "desc" }]
+      bool: {
+        // must: 关键词匹配 (会计算相关性权重)
+        must: [
+          {
+            match_phrase: { 
+              "message": "SignMpcTransaction" // 匹配短语，比 match 更精确
+            },
+            
+          },
+          {
+            match_phrase: { "type": "test" } 
+          },
+          {
+            match_phrase: { "message": "error" }
+          }
+        ],
+        // filter: 范围过滤 (不计算评分，速度快，支持缓存)
+        filter: [
+          {
+            range: {
+              "@timestamp": {
+                "gte": "now-24h", // gte: 大于等于；now-1h 表示 1 小时前
+                "lte": "now"    // lte: 小于等于
+              }
+            }
+          }
+        ]
+      }
+    }
   };
+
+
+
+// const queryData = {
+//         "query": {
+//             "bool": {
+//                 "must": [
+//                     {"match_phrase":{"type":net}},
+//                     //{"match_phrase":{"message": message}},
+//                     //{"match_phrase": {"message": tansaction}}
+
+//                 ],
+//                 "filter": [
+//                     {"range": {"@timestamp": {"gte": `now-${query_period}s"`}}}
+//                 ]
+//             }
+//         },
+//         //"from":0,
+//         "size":1,
+//         "sort": [
+//             {
+//                 "@timestamp": {
+//                     "order": "desc",
+//                     "unmapped_type": "long"
+//                 }
+//             }
+//         ],
+//         //"_source": {"include": ["type", "@timestamp","message"]}
+//     }
 
   try {
     const response = await axios.post(url, queryData, {
@@ -31,4 +88,4 @@ async function getLogs() {
   }
 }
 
-getLogs();
+getLogs("main","SignMpcTransaction",86400);
